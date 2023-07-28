ce0f97fd9f640f2984e4fd9f231d58cb8f403820
Manish_Syal <manishsyal77@gmail.com>
2023-07-29 00:21:00 +0530

    Designed Login & Register Screens UI part

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,118 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { KeyboardAvoidingView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        padding: 10,
+        alignItems: "center",
+      }}
+    >
+      <KeyboardAvoidingView>
+        <View
+          style={{
+            marginTop: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#4A55A2", fontSize: 17, fontWeight: "600" }}>
+            Sign In
+          </Text>
+
+          <Text style={{ fontSize: 17, fontWeight: "600", marginTop: 15 }}>
+            Sign In to Your Account
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 50 }}>
+          {/* Email View */}
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Email
+            </Text>
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                fontSize: email ? 18 : 15,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="enter Your Email"
+            />
+          </View>
+
+          {/* Password View */}
+          <View style={{ marginTop: 10 }}>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Password
+            </Text>
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                fontSize: email ? 18 : 15,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="enter Your Password"
+            />
+          </View>
+
+          {/* Login Button */}
+          <Pressable
+            onPress={handleLogin}
+            style={{
+              width: 200,
+              backgroundColor: "#4A55A2",
+              padding: 15,
+              marginTop: 50,
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: 6,
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Login
+            </Text>
+          </Pressable>
+
+          {/* Dont have account */}
+          <Pressable
+            onPress={() => navigation.navigate("Register")}
+            style={{ marginTop: 15 }}
+          >
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Dont't have an account? Sign Up
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,10 +1,165 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const RegisterScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        padding: 10,
+        alignItems: "center",
+      }}
+    >
+      <KeyboardAvoidingView>
+        <View
+          style={{
+            marginTop: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#4A55A2", fontSize: 17, fontWeight: "600" }}>
+            Register
+          </Text>
+
+          <Text style={{ fontSize: 17, fontWeight: "600", marginTop: 15 }}>
+            Register to Your Account
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 50 }}>
+          {/* Name View */}
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Name
+            </Text>
+            <TextInput
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={{
+                fontSize: name ? 18 : 15,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Enter Your Name"
+            />
+          </View>
+
+          {/* Email View */}
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Email
+            </Text>
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                fontSize: email ? 18 : 15,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Enter Your Email"
+            />
+          </View>
+
+          {/* Password View */}
+          <View style={{ marginTop: 10 }}>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Password
+            </Text>
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                fontSize: email ? 18 : 15,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Enter Your Password"
+            />
+          </View>
+
+          {/* Image */}
+          <View style={{ marginTop: 10 }}>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+              Image
+            </Text>
+            <TextInput
+              value={image}
+              onChangeText={(text) => setImage(text)}
+              style={{
+                fontSize: image ? 18 : 15,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              placeholderTextColor={"black"}
+              placeholder="Image"
+            />
+          </View>
+
+          {/* Sign Up Button */}
+          <Pressable
+            //onPress={handleLogin}
+            style={{
+              width: 200,
+              backgroundColor: "#4A55A2",
+              padding: 15,
+              marginTop: 50,
+              marginLeft: "auto",
+              marginRight: "auto",
+              borderRadius: 6,
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Register
+            </Text>
+          </Pressable>
+
+          {/* Dont have account */}
+          <Pressable
+            onPress={() => navigation.goBack()}
+            style={{ marginTop: 15 }}
+          >
+            <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+              Already have an account? Sign In
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
