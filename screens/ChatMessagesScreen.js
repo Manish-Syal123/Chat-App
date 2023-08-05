d8d0442bf2e65ddc24cdcd988a3f569a3039b40c
Manish_Syal <manishsyal77@gmail.com>
2023-08-05 18:48:19 +0530

    Added the functionality for selecting the emoji using EmojiSelector

--- screens/ChatMessagesScreen.js
+++ screens/ChatMessagesScreen.js
@@ -1,70 +1,89 @@
 import {
-    KeyboardAvoidingView,
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Pressable,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Entypo, MaterialCommunityIcons, Feather } from "@expo/vector-icons";
+import EmojiSelector from "react-native-emoji-selector";
+
 const ChatMessagesScreen = () => {
-    return (
-        <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "#F0F0F0" }}>
-            <ScrollView>{/* All the chat messages go over here */}</ScrollView>
+  const [showEmojiSelector, setShowEmojiSelector] = useState(false);
+  const [message, setMessage] = useState(""); // simple text typed by the user inside the TextInput field
 
-            <View
-                style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    paddingHorizontal: 5,
-                    paddingVertical: 1,
-                    borderTopWidth: 1,
-                    borderTopColor: "#dddddd",
-                    marginBottom: 8,
-                }}
-            >
-                <Entypo
-                    style={{ marginRight: 5 }}
-                    name="emoji-happy"
-                    size={24}
-                    color="gray"
-                />
-                <TextInput
-                    style={{
-                        flex: 1,
-                        height: 40,
-                        borderWidth: 1.5,
-                        borderColor: "#dddddd",
-                        borderRadius: 20,
-                        paddingHorizontal: 10,
-                        backgroundColor: "white",
-                    }}
-                    placeholder="Type your message..."
-                />
+  const handleEmojiPress = () => {
+    setShowEmojiSelector(!showEmojiSelector);
+  };
+  return (
+    <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "#F0F0F0" }}>
+      <ScrollView>{/* All the chat messages go over here */}</ScrollView>
 
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        paddingHorizontal: 4,
-                        paddingVertical: 10,
-                        //borderTopWidth: 1,
-                        borderTopColor: "#dddddd",
-                        gap: 5,
-                    }}
-                >
-                    <Entypo name="camera" size={24} color="gray" />
-                    <Feather name="mic" size={24} color="gray" />
-                </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: 5,
+          paddingVertical: 1,
+          borderTopWidth: 1,
+          borderTopColor: "#dddddd",
+          marginBottom: showEmojiSelector ? 0 : 8,
+        }}
+      >
+        <Entypo
+          onPress={handleEmojiPress}
+          style={{ marginRight: 5 }}
+          name="emoji-happy"
+          size={24}
+          color="gray"
+        />
+        <TextInput
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+          style={{
+            flex: 1,
+            height: 40,
+            borderWidth: 1.5,
+            borderColor: "#dddddd",
+            borderRadius: 20,
+            paddingHorizontal: 10,
+            backgroundColor: "white",
+          }}
+          placeholder="Type your message..."
+        />
 
-                <Pressable>
-                    <MaterialCommunityIcons name="send-circle" size={60} color="green" />
-                </Pressable>
-            </View>
-        </KeyboardAvoidingView>
-    );
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            paddingHorizontal: 4,
+            paddingVertical: 10,
+            borderTopColor: "#dddddd",
+            gap: 5,
+          }}
+        >
+          <Entypo name="camera" size={24} color="gray" />
+          <Feather name="mic" size={24} color="gray" />
+        </View>
+
+        <Pressable>
+          <MaterialCommunityIcons name="send-circle" size={60} color="green" />
+        </Pressable>
+      </View>
+
+      {showEmojiSelector && (
+        <EmojiSelector
+          onEmojiSelected={(emoji) => {
+            setMessage((prevMessage) => prevMessage + emoji);
+          }}
+          style={{ height: 250 }}
+        />
+      )}
+    </KeyboardAvoidingView>
+  );
 };
 
 export default ChatMessagesScreen;
